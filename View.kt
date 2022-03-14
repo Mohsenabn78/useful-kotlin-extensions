98766b76596c13ddf2b34d4b46c0f5ebcc33f6de
mohsen <mohsenabedini79OO@gmail.com>
2022-03-14 13:10:51 +0330

    initial comit

---
+++ View.kt
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2020 Fernando Cejas Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ir.baron.app.core.extension
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.FragmentActivity
+import coil.ImageLoader
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.target.BaseTarget
+import com.bumptech.glide.request.target.SizeReadyCallback
+import com.bumptech.glide.request.transition.Transition
+
+fun View.cancelTransition() {
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+        transitionName = null
+    }
+}
+
+fun View.isVisible() = this.visibility == View.VISIBLE
+
+fun View.visible() { this.visibility = View.VISIBLE }
+
+fun View.invisible() { this.visibility = View.GONE }
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View =
+        LayoutInflater.from(context).inflate(layoutRes, this, false)
+
+fun ImageView.loadFromUrl(url: String) =
+    Glide.with(this.context.applicationContext)
+        .load(url)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(this)
+
+fun ImageView.loadUrlAndPostponeEnterTransition(url: String, activity: FragmentActivity) {
+    val target: ImageViewBaseTarget = ImageViewBaseTarget(this, activity)
+    Glide.with(context.applicationContext).load(url).into(target)
+}
+
+fun ImageView.loadSvgUrl(url: String,duration:Int=0,placeHolder:Drawable?=null) {
+
+    val imageLoader = ImageLoader.Builder(this.context)
+        .componentRegistry { add(SvgDecoder(this@loadSvgUrl.context)) }
+        .build()
+
+    val request = ImageRequest.Builder(this.context)
+        .crossfade(true)
+        .crossfade(100)
+        .placeholder(placeHolder)
+        //.error(R.drawable.error)
+        .data(url)
+        .target(this)
+        .build()
+
+    imageLoader.enqueue(request)
+}
+
+private class ImageViewBaseTarget (var imageView: ImageView?, var activity: FragmentActivity?) : BaseTarget<Drawable>() {
+    override fun onLoadFailed(errorDrawable: Drawable?) {
+        super.onLoadFailed(errorDrawable)
+        activity?.supportStartPostponedEnterTransition()
+    }
+
+    override fun getSize(cb: SizeReadyCallback) = cb.onSizeReady(SIZE_ORIGINAL, SIZE_ORIGINAL)
+
+    override fun removeCallback(cb: SizeReadyCallback) {
+        imageView = null
+        activity = null
+    }
+
+    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+        imageView?.setImageDrawable(resource)
+        activity?.supportStartPostponedEnterTransition()
+    }
+}
