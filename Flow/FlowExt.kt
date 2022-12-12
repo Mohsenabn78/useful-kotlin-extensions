35be7605bc9384c086dfda923a7c4761c4f63299
mohsen <mohsenabedini79OO@gmail.com>
2022-12-12 22:22:18 +0330

    add flow extension

---
+++ Flow/FlowExt.kt
@@ -0,0 +1,27 @@
+
+// for see more extensions check https://github.com/Mohsenabn78/useful-kotlin-extensions
+
+inline fun <T> Flow<T>.launchAndCollectIn(
+    owner: LifecycleOwner,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    crossinline action: suspend CoroutineScope.(T) -> Unit
+) = owner.lifecycleScope.launch {
+    owner.repeatOnLifecycle(minActiveState) {
+        collect {
+            action(it)
+        }
+    }
+}
+
+
+inline fun <T> Flow<T>.launchAndCollectLast(
+    owner: LifecycleOwner,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    crossinline action: suspend CoroutineScope.(T) -> Unit
+) = owner.lifecycleScope.launch {
+    owner.repeatOnLifecycle(minActiveState) {
+        collectLatest {
+            action(it)
+        }
+    }
+}
