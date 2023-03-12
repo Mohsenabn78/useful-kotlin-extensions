abd73192f776472fc42202327816688be897741c
Ali Dehkhodaei <alidehkhodaei1998@gmail.com>
2023-03-12 19:56:27 +0330

    Add functions for calculating date differences (#1)
    This commit adds three new functions to calculate the absolute difference in days, years, and months between two LocalDate objects.

--- Date/DateExt.kt
+++ Date/DateExt.kt
@@ -95,3 +95,54 @@ fun Date.dayOfWeekName(locale: Locale? = Locale.getDefault()): String {
 fun Date.dayOfYear(): Int {
     return this.toCalendar().get(Calendar.DAY_OF_YEAR)
 }
+
+/**
+ * This function calculates the number of years between two LocalDate objects.
+ * It uses the ChronoUnit.YEARS enum value to calculate the difference in years between the two dates.
+ * If the result is negative, it returns the absolute value of the difference.
+ *
+ * @param other: The other LocalDate object to calculate the difference from.
+ * @return The number of years between the two LocalDate objects as a Long data type.
+ */
+fun LocalDate.yearsDifference(other: LocalDate): Long {
+    return ChronoUnit.YEARS.between(this, other).absoluteValue
+}
+
+/**
+ * This function calculates the number of months between two LocalDate objects.
+ * It uses the ChronoUnit.MONTHS enum value to calculate the difference in months between the two dates.
+ * If the result is negative, it returns the absolute value of the difference.
+ *
+ * @param other: The other LocalDate object to calculate the difference from.
+ * @return The number of months between the two LocalDate objects as a Long data type.
+ */
+fun LocalDate.monthsDifference(other: LocalDate): Long {
+    return ChronoUnit.MONTHS.between(this, other).absoluteValue
+}
+
+/**
+ * This function calculates the number of days between two LocalDate objects.
+ * It uses the ChronoUnit.DAYS enum value to calculate the difference in days between the two dates.
+ * If the result is negative, it returns the absolute value of the difference.
+ *
+ * @param  other: The other LocalDate object to calculate the difference from.
+ * @return The number of days between the two LocalDate objects as a Long data type.
+ */
+fun LocalDate.daysDifference(other: LocalDate): Long {
+    return ChronoUnit.DAYS.between(this, other).absoluteValue
+}
+
+/*
+Example:
+
+fun main() {
+
+    val date1 = LocalDate.of(1998, 9, 24)
+    val date2 = LocalDate.of(2023, 3, 12)
+
+    println(date1.yearsDifference(date2)) // 24
+    println(date1.monthsDifference(date2)) // 293
+    println(date1.daysDifference(date2)) // 8935
+    
+}
+ */
